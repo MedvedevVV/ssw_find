d4fff8d301d91c8d176a8377943186a9e12584f2
MedvedevVV <107478820+MedvedevVV@users.noreply.github.com>
2022-08-20 13:15:42 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<html lang="ru">
+<head>
+    <title>Панель доменов</title>
+    
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+    
+    <script type="text/javascript" src="script.js"></script>
+    
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <form autocomplete = "off">
+        
+        <label>
+            <input type="text" id="search" placeholder="Введите домен" />
+        </label>
+    </form>
+   
+    <div id="display"></div>
+</body>
+</html>
