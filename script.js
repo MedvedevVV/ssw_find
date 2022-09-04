5626014bd7914980295f0e9122d2f027e14254c7
MedvedevVV <107478820+MedvedevVV@users.noreply.github.com>
2022-09-04 10:08:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
 $(document).ready(function() {
 
+    // keyup обрабатывает события. Когда user нажимает кнопку, вводя значения в поле - он срабатывает
+    // В файле index.php полю для поиска присвоен id='search'
 
     $("#search").keyup(function() {
-
+        
+   // Записываем в переменную 'name' значение из поля для поиска
  
         var name = $('#search').val();
-
-
+        
+    // Проверяем значение переменной на пустоту
+        
         if (name === "") {
-
+            
+    //Если 'name' пустая, то блок div c id = 'display' будет очищен
 
             $("#display").html("");
 
         }
         else {
-
+    //Иначе вызываем ajax - функцию
 
             $.ajax({
 
-                type: "POST", 
-                url: "script.php", 
+                type: "POST", //Тип запроса
+                url: "script.php", //Путь к обработчику
                 data: {
                     
                     search: name 
                 },
                 success: function(response) {
-                    
+                    // Если запрос успешный, то добавляем результат в div с id = 'dispay'
                     $("#display").html(response).show();
                 }
 
@@ -38,10 +43,10 @@ $(document).ready(function() {
 });
 
 function fill(Value) {
+    // Эта функция обрабатывает клик мышью по результам выдачи поиска
 
+    $('#search').val(Value); //Добавляем в поле поиска значение результата
 
-    $('#search').val(Value); 
-
-    $('#display').hide(); 
+    $('#display').hide(); //Скрываем остальные результаты
 
 }
